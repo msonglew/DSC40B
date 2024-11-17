51d8435ee7e16c2533ceffa0bb6a6f389ca3133b
msonglew <msonglew@gmail.com>
2024-11-17 11:34:27 -0800

    implement

--- biggest_descendent.py
+++ biggest_descendent.py
@@ -19,7 +19,7 @@ def biggest_descendent(graph, root, value):
 	>>> edges = [(1, 2), (1, 3), (2, 4), (2, 5), (4, 8), (4, 9), (3, 6), (3, 7)]
 	>>> g = dsc40graph.DirectedGraph()
 	>>> for edge in edges: g.add_edge(*edge)
-	value = {1: 2, 2: 1, 3: 4, 4: 8, 5: 5, 6: 2, 7: 10, 8: 3, 9: 9}
+	>>> value = {1: 2, 2: 1, 3: 4, 4: 8, 5: 5, 6: 2, 7: 10, 8: 3, 9: 9}
 	>>> biggest_descendent(g, 1, value)
 	{1: 10, 2: 9, 3: 10, 4: 9, 5: 5, 6: 2, 7: 10, 8: 3, 9: 9}
 
@@ -27,7 +27,7 @@ def biggest_descendent(graph, root, value):
 	>>> edges = [(1, 2), (1, 3), (2, 4), (2, 5), (4, 8), (4, 9), (3, 6), (3, 7)]
 	>>> g = dsc40graph.DirectedGraph()
 	>>> for edge in edges: g.add_edge(*edge)
-	value = {1: 100, 2: 1, 3: 4, 4: 8, 5: 5, 6: 2, 7: 10, 8: 3, 9: 9}
+	>>> value = {1: 100, 2: 1, 3: 4, 4: 8, 5: 5, 6: 2, 7: 10, 8: 3, 9: 9}
 	>>> biggest_descendent(g, 1, value)
 	{1: 100, 2: 9, 3: 10, 4: 9, 5: 5, 6: 2, 7: 10, 8: 3, 9: 9}
 
@@ -35,7 +35,7 @@ def biggest_descendent(graph, root, value):
 	>>> edges = [(1, 2), (1, 3), (2, 4), (2, 5), (4, 8), (4, 9), (3, 6), (3, 7)]
 	>>> g = dsc40graph.DirectedGraph()
 	>>> for edge in edges: g.add_edge(*edge)
-	value = {1: 2, 2: 100, 3: 4, 4: 8, 5: 5, 6: 2, 7: 10, 8: 3, 9: 9}
+	>>> value = {1: 2, 2: 100, 3: 4, 4: 8, 5: 5, 6: 2, 7: 10, 8: 3, 9: 9}
 	>>> biggest_descendent(g, 1, value)
 	{1: 100, 2: 100, 3: 10, 4: 9, 5: 5, 6: 2, 7: 10, 8: 3, 9: 9}
 
@@ -43,10 +43,37 @@ def biggest_descendent(graph, root, value):
 	>>> edges = [(1, 2), (1, 3), (1, 4)]
 	>>> g = dsc40graph.DirectedGraph()
 	>>> for edge in edges: g.add_edge(*edge)
-	value = {1: 2, 2: 1, 3: 4, 4: 8}
+	>>> value = {1: 2, 2: 1, 3: 4, 4: 8}
 	>>> biggest_descendent(g, 1, value)
 	{1: 8, 2: 1, 3: 4, 4: 8}
-	
+
 	"""
 
 	# use dfs implementation
+
+	def dfs(graph, u, value, status=None):
+		"""Start a DFS at 'u'."""
+		# initialize status if it was not passed
+		if status is None:
+			status = {node: 'undiscovered' for node in graph.nodes}
+
+		status[u] = 'pending'
+		child_values = list()
+		child_values.append(value[u])
+
+		for v in graph.neighbors(u): # explore edge (u, v)
+			if status[v] == 'undiscovered':
+				
+				dfs(graph, v, value, status)
+				child_values.append(value[v])
+
+		value[u] = max(child_values)
+
+		# print("call to dfs -----------")
+		# print("child_values: "+str(child_values))
+		# print("max: "+str(max(child_values)))
+
+
+	dfs(graph, root, value)
+
+	return value
