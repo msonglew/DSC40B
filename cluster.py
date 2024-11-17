27c6b750a7a88bb3e5ed576923d607ec2e39f1a0
msonglew <msonglew@gmail.com>
2024-11-17 12:16:23 -0800

    test cases

--- cluster.py
+++ cluster.py
@@ -18,9 +18,32 @@ def cluster(graph, weights, level):
 			inner frozensets should contain notes in a cluster
 
 	# general case
+	>>> edges = [('a', 'b'), ('b', 'c'), ('c', 'd'), ('d', 'a')]
+	>>> graph = dsc40graph.UndirectedGraph()
+	>>> for edge in edges: graph.add_edge(*edge)
 	>>> def weights(x, y):
 	... x, y = (x, y) if x < y else (y, x)
 	... return {("a", "b"): 1, ("b", "c"): .3, ("c", "d"): .9, ("a", "d"): .2}[(x, y)]
-	>>> cluster.cluster(graph, weights, 0.4)
+	>>> cluster(graph, weights, 0.4)
 	frozenset([frozenset(['a', 'b']), frozenset(['c', 'd'])])
+
+	# weight makes unconnected graph
+	>>> cluster(graph, weights, 1)
+	frozenset([frozenset(['a', 'b']), frozenset(['c']), frozenset(['d'])])
+
+	# unconnected graph
+	>>> graph.add_edge('e', 'f')
+	>>> def weights(x, y):
+	... x, y = (x, y) if x < y else (y, x)
+	... return {("a", "b"): 1, ("b", "c"): .3, ("c", "d"): .9, ("a", "d"): .2, ("e", "f"): .5}[(x, y)]
+	>>> cluster(graph, weights, 0.4)
+	frozenset([frozenset(['a', 'b']), frozenset(['c', 'd']), frozenset(['e', 'f'])])
+	
 	"""
+
+	# implement a modified full bfs/dfs with with a guard case
+	# i.e. continue only if edge weight is valid
+
+	
+
+
